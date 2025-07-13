c2c566bd22657ce4f06930c6073977a889b6c343
Thuyh0h <trongthuynl2004@gmail.com>
2025-07-13 15:07:09 +0700

    ListView Templates

---
+++ Tutorial-Templates/WindowsFormsApp_ListView/WindowsFormsApp_ListView/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApp_ListView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(40, 48);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(532, 361);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(628, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Small_items";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(628, 299);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 41);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Details";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(628, 201);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 41);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Large_items";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Tutorial-Templates/WindowsFormsApp_ListView/WindowsFormsApp_ListView/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace WindowsFormsApp_ListView
+{
+    public partial class Form1 : Form
+    {
+
+        ImageList SmallImageList;
+        ImageList LargeImageList;
+
+        public Form1()
+        {
+            InitializeComponent();
+            loadImageLists();
+            loadListView();
+        }
+
+        void loadImageLists()
+        {
+            SmallImageList = new ImageList() { ImageSize = new Size(16,16)};
+           SmallImageList.Images.Add(new Bitmap(Application.StartupPath + "\\Images\\1.png"));
+            SmallImageList.Images.Add(new Bitmap(Application.StartupPath + "\\Images\\2.png"));
+            SmallImageList.Images.Add(new Bitmap(Application.StartupPath + "\\Images\\3.png"));
+
+            LargeImageList = new ImageList() { ImageSize = new Size(68, 68) };
+            LargeImageList.Images.Add(new Bitmap(Application.StartupPath + "\\Images\\1.png"));
+            LargeImageList.Images.Add(new Bitmap(Application.StartupPath + "\\Images\\2.png"));
+            LargeImageList.Images.Add(new Bitmap(Application.StartupPath + "\\Images\\3.png"));
+        }
+
+        void loadListView()
+        {
+            listView1.Columns.Add("Columns 1", 100);
+            listView1.Columns.Add("Columns 2", 100);
+            listView1.Columns.Add("Columns 3", 100);
+
+            listView1.FullRowSelect = true;
+
+            ListViewItem item1 = new ListViewItem("Item 1");  
+            item1.ImageIndex = 0; // Set the image index for the first item
+            item1.SubItems.Add("1 SubItem 1");
+            item1.SubItems.Add("1 SubItem 2");
+            listView1.Items.Add(item1);
+
+            ListViewItem item2 = new ListViewItem("Item 2");
+            item2.ImageIndex = 1; // Set the image index for the second item
+            item2.SubItems.Add("2 SubItem 1");
+            item2.SubItems.Add("2 SubItem 2");
+            listView1.Items.Add(item2);
+
+            ListViewItem item3 = new ListViewItem("Item 3");
+            item3.ImageIndex = 2; // Set the image index for the third item
+            item3.SubItems.Add("2 SubItem 1");
+            item3.SubItems.Add("2 SubItem 2");
+            listView1.Items.Add(item3);
+        }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.SmallIcon;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listView1.View = View.LargeIcon;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            System.Windows.Forms.ListView lv = sender as System.Windows.Forms.ListView;
+
+            if (lv.SelectedItems.Count > 0)
+            {
+                foreach (ListViewItem item in lv.SelectedItems)
+                {
+                    MessageBox.Show("Selected Item: " + item.Text);
+                }
+            }
+        }
+    }
+}
